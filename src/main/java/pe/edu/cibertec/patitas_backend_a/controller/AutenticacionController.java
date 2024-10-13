576c2b87c3ebe3d7d545eebe1cc01c9fc88f8d31
jere7612 <70173572+jere7612@users.noreply.github.com>
2024-10-12 22:19:17 -0500

    mod

--- src/main/java/pe/edu/cibertec/patitas_backend_a/controller/AutenticacionController.java
+++ src/main/java/pe/edu/cibertec/patitas_backend_a/controller/AutenticacionController.java
@@ -1,12 +1,12 @@
 package pe.edu.cibertec.patitas_backend_a.controller;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import pe.edu.cibertec.patitas_backend_a.dto.LoginRequestDTO;
 import pe.edu.cibertec.patitas_backend_a.dto.LoginResponseDTO;
 import pe.edu.cibertec.patitas_backend_a.service.AutenticacionService;
-
 import java.io.IOException;
+import java.time.Duration;
+import java.util.Arrays;
 
 @RestController
 @RequestMapping("/autenticacion")
@@ -19,23 +19,19 @@ public class AutenticacionController {
     public LoginResponseDTO login(@RequestBody LoginRequestDTO loginRequestDTO) {
 
         try {
-
+            Thread.sleep(Duration.ofSeconds(60));
             String[] datosUsuario = autenticacionService.validarUsuario(loginRequestDTO);
+            System.out.println("Resultado: " + Arrays.toString(datosUsuario));
             if (datosUsuario == null) {
-                return new LoginResponseDTO("01", "Error: Usuario no entrado","Brunp Diaz","ciudadgoticac@gmail.com");
+                return new LoginResponseDTO("01", "Error: Usuario no entrado", "", "");
 
             }
             return new LoginResponseDTO("00", "", datosUsuario[0], datosUsuario[1]);
 
-        } catch (IOException e) {
-            return new LoginResponseDTO("99", "Error: Ocurrio un problema","Brunp Diaz","ciudadgoticac@gmail.com");
-
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            return new LoginResponseDTO("99", "Error: Ocurrio un problema", "", "");
+
+
         }
-
-
     }
-
-
 }
